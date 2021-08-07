c2f18829668f922533d867489fc6908086db84b6
NetLSS <futureprogrammer@naver.com>
2021-08-07 14:23:53 +0900

    Feat: activity life-cycle func override

--- app/src/main/java/com/lilcode/example/statechange/MainActivity.kt
+++ app/src/main/java/com/lilcode/example/statechange/MainActivity.kt
@@ -2,15 +2,61 @@ package com.lilcode.example.statechange
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.lilcode.example.statechange.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
 
+    companion object {
+        const val TAG = "StateChange"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        Log.i(TAG, "onCreate()")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop()")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy()")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.i(TAG, "onSaveInstanceState()")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.i(TAG, "onRestoreInstanceState()")
     }
 }
